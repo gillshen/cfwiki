506046c5e944555ebdee3912c0fbcdd66d022815
Gill Shen <gillarete@gmail.com>
2024-07-21 12:09:25 +0800

    Add the Contract model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models import Case, When, Value
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 from .managers import CFUserManager
 
@@ -41,3 +43,67 @@ class Student(models.Model):
             return f"{self.lastname}{self.firstname}"
         else:
             return f"{self.firstname} {self.lastname}"
+
+    @property
+    def contracts_sorted(self):
+        """
+        Return related contracts sorted by
+
+            - `status_code` (effective ones first),
+            - `target_year` (descending), and
+            - `date` (descending, null last)
+
+        so that the first in the sorted QuerySet will be the latest of
+        the effective contract if there are effective contracts, or else
+        the latest of the non-effective contracts.
+        """
+        contracts = self.contracts.annotate(
+            status_code=Case(
+                When(status=Contract.Status.IN_EFFECT, then=Value(0)),
+                default=Value(1),
+                output_field=models.IntegerField(),
+            ),
+            date_code=Case(
+                When(date__isnull=True, then=Value(1)),
+                default=Value(0),
+                output_field=models.IntegerField(),
+            ),
+        )
+        return contracts.order_by(
+            "status_code",
+            "-target_year",
+            "date_code",
+            "-date",
+        )
+
+
+class Contract(models.Model):
+
+    class Status(models.TextChoices):
+        IN_EFFECT = "In effect", _("In effect")
+        FULFILED = "Fulfilled", _("Fulfilled")
+        TERMINATED = "Terminated", _("Terminated")
+
+    student = models.ForeignKey(
+        Student,
+        related_name="contracts",
+        on_delete=models.CASCADE,
+    )
+    type = models.CharField(max_length=100)
+    target_year = models.IntegerField()
+    date = models.DateField(blank=True, null=True)
+    status = models.CharField(max_length=50, choices=Status.choices)
+    student_initial_progression = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                "student",
+                "type",
+                "target_year",
+                name="contract_unique_student_type_targetyear",
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.student} >> {self.type}"
--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
-from core.models import Student
+from core.models import Student, Contract
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    class ContractSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Contract
+            exclude = ["student"]
+
     class Meta:
         model = Student
         fields = "__all__"
 
     fullname = serializers.CharField()
+    contracts_sorted = ContractSerializer(many=True)
